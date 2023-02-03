9dd5abb6b861afb292834724cffbb0d1166d5eb2
sidiropulo <123432117+sidiropulo@users.noreply.github.com>
2023-02-04 01:46:26 +0300

    Update задание2.sql

--- 2 task/задание2.sql
+++ 2 task/задание2.sql
@@ -1,34 +1,88 @@
-
-
+----------------------------------------------------------------------
+-- таблица
+----------------------------------------------------------------------
 create table jsonb_test (
            id serial primary key, 
 		   data jsonb
 )
 
-разработать таблицу  в БД для хранения статичных данных используемых затем в приложениях для показа в интерфейсе пользователя 
-как текстовые поля, а так же как имена различных контролов.
-Например:
- необходимо в пользовательском интерфейсе
- вывести в label  фразу "Дата выпуска", 
-а свойство Name  для label назначить "date1"., 
+----------------------------------------------------------------------
+--                  индекс на поле button_id (название кнопки)
+----------------------------------------------------------------------
 
-так вот как это хранить в бд, 
-если таких записей может быть много и они используются для построения динамического пользовательского интерфейса.
-Подсказка: json.
-
-вставки, изменения, удаления.
+CREATE INDEX ON jsonb_test((data->>'button_id'));
 
 
+----------------------------------------------------------------------
+--                  вставка в таблицу
+----------------------------------------------------------------------
 INSERT INTO jsonb_test (data) VALUES 
-('{"button_id": "label1", "config": {"name": "date2", "textval": "Меню"}}')
-
-
-INSERT INTO jsonb_test (data) VALUES 
+('{"button_id": "label", "name": "date1", "textval": "Дата выпуска"}'),
 ('{"button_id": "pushbutton", "name": "click", "textval": "Завершить"}')
 
 
-        
+----------------------------------------------------------------------
+--                   функция добавления в таблицу
+----------------------------------------------------------------------
+ create or replace function created_json(str jsonb)
+  returns text
+  language plpgsql
+  as $$
+    begin
+	   INSERT INTO jsonb_test (data) VALUES 
+       ( to_json(str)::jsonb);
+      return str;
+    end
+  $$;
+  
+select * from  created_json
+('{"button_id": "label", "name": "date1", "textval": "Дата"}')  
+
+
+----------------------------------------------------------------------
+--                    функция удаления по названию кнопки 
+----------------------------------------------------------------------
+
+create or replace function delete_json_by_button(str text)
+  returns text
+  language plpgsql
+  as $$
+    begin
+	   delete from jsonb_test  where data ->> 'button_id' = str;
+      return str;
+    end
+  $$;
+
+select * from delete_json_by_button ('label') 
+
+----------------------------------------------------------------------
+--                   функция удаления по id
+----------------------------------------------------------------------
+
+
+create or replace function delete_json_by_id(str int)
+  returns int
+  language plpgsql
+  as $$
+    begin
+	   delete from jsonb_test  where id = str;
+      return str;
+    end
+  $$;
+select * from delete_json_by_id (1) 
+----------------------------------------------------------------------
+--                   функция обновления по id
+----------------------------------------------------------------------
+
+create or replace function update_json_by_id(number int, str jsonb)
+  returns text
+  language plpgsql
+  as $$
    
-SELECT * FROM jsonb_test WHERE data ->> 'textval' = 'Дата выпуска';
+    begin
+	   UPDATE jsonb_test
+	   set data =  ( to_json(str)) where id = number;
+      return str;
+    end
+  $$;
  
- SELECT * FROM jsonb_test
